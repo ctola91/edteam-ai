a0f6d8e70b9e2559c778ca4c601e42431fb6784a
ctola91 <christian.tola@hotmail.com>
2024-10-19 22:00:49 -0400

    Add First page

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,18 @@
 <script setup lang="ts">
-import { useGeminiStore } from '~/store/gemini';
-
-const questions = ref([])
-
-const gemini = useGeminiStore()
-const { data, loading } = storeToRefs(gemini)
-
-onMounted(async () => {
-  await gemini.loadGeminiData()
-  if(data.value) {
-    questions.value = data.value
-  }
-})
+import Logo from "/logo.png"
 </script>
 <template>
-  Home Page
-  <div v-if="loading">
-    Cargando...
-  </div>
-  <div v-else>
-    <div v-for="q in questions" :key="q.pregunta">{{ q.pregunta }} <br />
-      {{  q.respuestas }}<br>
-      {{ q.correcta }}
-    </div>
-  </div>
+  <UContainer as="div" class="p-4 flex justify-center align-center">
+    <UCard class="w-80">
+      <div>
+        <img :src="Logo" alt="logo" width="320" />
+      </div>
+      <template #footer>
+        <div class="flex flex-row justify-between gap-2 flex-wrap">
+          <UButton size="lg" color="indigo" label="Iniciar" block to="/game" />
+          <UButton size="lg" color="gray" label="Generar Preguntas" block to="questions" />          
+        </div>
+      </template>
+    </UCard>
+  </UContainer>
 </template>
