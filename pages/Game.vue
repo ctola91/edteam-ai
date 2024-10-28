4adfdd76e69a014ace4e7a00d46e9451241fff4e
ctola91 <christian.tola@hotmail.com>
2024-10-27 21:25:05 -0400

    Add Game page

--- pages/Game.vue
+++ pages/Game.vue
@@ -1,3 +1,37 @@
+<script lang="ts" setup>
+import { useGeminiStore } from '~/store/gemini';
+
+const store = useGeminiStore()
+const { categorySelected, topicSelected, data } = storeToRefs(store)
+
+const questionIndex = ref(0)
+
+const questions = computed(() => {
+  let result = []
+  const cat = data.value.categories.find(i => i.id === categorySelected.value)
+  if (cat) {
+    let top = cat.topics.find(i => i.id === topicSelected.value)
+    result = top.data
+  }
+  return result
+})
+</script>
+
 <template>
-  Game
+  <template v-for="(question, index) in questions" :key="question.pregunta">
+    <template v-if="index === questionIndex">
+      <div class="flex flex-col gap-8">
+      <UCard>
+        <div class="flex flex-col gap-2">
+          <h1 class="text-4xl text-center font-bold">{{ question.pregunta }}</h1>
+        </div>
+      </UCard>
+      <div class="flex gap-2 justify-between w-3/4 m-auto">
+        <UButton color="gray" size="xl" v-for="respuesta in question.respuestas" :key="respuesta">
+          {{ respuesta }}
+        </UButton>
+      </div>
+    </div>
+    </template>
+  </template>
 </template>
